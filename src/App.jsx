46c08b6cddca2824dcf3d58296791173b35e92cd
Kuadrao <lucianoonatevenegas@gmail.com>
2023-06-22 22:29:52 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { Header } from './components/Header';
+import { ProductList } from './components/ProductList';
+import { data } from './data';
+
+
+function App() {
+  const [allProducts, setAllProducts] = useState(data);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
+
+  return (
+    <>
+      <Header
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+      <ProductList
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.jsx
@@ -0,0 +1,45 @@
+export const data = [
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1577221084712-45b0445d2b00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=698&q=80',
+    nameEjercicio: 'Zapatos Nike',
+    descripcion: 'Descripción del ejercicio 1',
+    repeticiones: 10,
+  },
+  {
+    id: 2,
+    img: 'https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80',
+    nameEjercicio: 'Reloj',
+    descripcion: 'Descripción del ejercicio 2',
+    repeticiones: 8,
+  },
+  {
+    id: 3,
+    img: 'https://images.unsplash.com/photo-1605296867304-46d5465a13f1?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=870&amp;q=80',
+    nameEjercicio: 'Smartwatch',
+    descripcion: 'Descripción del ejercicio 3',
+    repeticiones: 12,
+  },
+  {
+    id: 4,
+    img: 'https://plus.unsplash.com/premium_photo-1679635697818-2cdd42cbc6e0?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=870&amp;q=80',
+    nameEjercicio: 'Perfume',
+    descripcion: 'Descripción del ejercicio 4',
+    repeticiones: 15,
+  },
+  {
+    id: 5,
+    img: 'https://images.unsplash.com/photo-1593494418158-38517e0b23ef?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=388&amp;q=80',
+    nameEjercicio: 'Audífonos',
+    descripcion: 'Descripción del ejercicio 5',
+    repeticiones: 20,
+  },
+  {
+    id: 6,
+    img: 'https://images.unsplash.com/photo-1601422407692-ec4eeec1d9b3?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=725&amp;q=80',
+    nameEjercicio: 'Nombre del Ejercicio',
+    descripcion: 'Descripción del ejercicio',
+    repeticiones: 10,
+  },
+  
+];
